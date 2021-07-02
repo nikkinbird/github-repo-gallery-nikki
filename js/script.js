5f96b47295a37a63e89d82ff4179d725d3af3194
nikkinbird <nikki.bollman@gmail.com>
2021-07-01 21:21:54 -0500

    Added function to display user info

--- js/script.js
+++ js/script.js
@@ -10,6 +10,24 @@ const getData = async function () {
 
     const data = await res.json();
     console.log(data);
+    displayUserInfo(data);
 };
 
 getData();
+
+// Function to display fetched user data
+const displayUserInfo = function(data) {
+    let userInfo = document.createElement("div");
+    userInfo.classList.add("user-info");
+    userInfo.innerHTML = 
+  `<figure>
+    <img alt="user avatar" src=${data.avatar_url} />
+  </figure>
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div>`
+  overview.append(userInfo);
+}
